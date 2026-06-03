ed0602bd5f77338c27b4028cb168a58bc65025b8
Fiqriananda <fiqrianandahakin@gmail.com>
2026-06-03 19:58:30 +0700

    feat: add status class

--- DTOs/UpdatePaketDto.cs
+++ DTOs/UpdatePaketDto.cs
@@ -1,17 +1,6 @@
+using static EkspedisiPaketAPI.Models.StatusPengiriman.StatusPengirimanConstant;
 namespace ExpedisiPaketAPI.DTOs
 {
-
-    namespace EkspedisiPaketAPI.Models
-    {
-        public static class StatusPengirimanConstant
-        {
-            public const string Pending = "PENDING";
-            public const string Diproses = "DIPROSES";
-            public const string Dikirim = "DIKIRIM";
-            public const string Selesai = "SELESAI";
-            public const string Batal = "BATAL";
-        }
-    }
     public class UpdatePaketDto
     {
         public string Pengirim { get; set; } = string.Empty;
--- Models/Paket.cs
+++ Models/Paket.cs
@@ -1,16 +1,7 @@
 using System;
-
+using static EkspedisiPaketAPI.Models.StatusPengiriman.StatusPengirimanConstant;
 namespace ExpedisiPaketAPI.Models
 {
-    public static class StatusPengirimanConstant
-    {
-        public const string Pending = "PENDING";
-        public const string Diproses = "DIPROSES";
-        public const string Dikirim = "DIKIRIM";
-        public const string Selesai = "SELESAI";
-        public const string Batal = "BATAL";
-    }
-
     public class Paket
     {
         public int Id { get; set; }
@@ -20,7 +11,7 @@ namespace ExpedisiPaketAPI.Models
         public string AlamatAsal { get; set; } = string.Empty;
         public string AlamatTujuan { get; set; } = string.Empty;
         public double BeratKg { get; set; }
-        public string StatusPengiriman { get; set; } = StatusPengirimanConstant.Pending;
+        public string StatusPengiriman { get; set; } = Pending;
         public DateTime TanggalDikirm { get; set; }
         public DateTime? TanggalDiterima { get; set; }
         public string Kota { get; set; } = string.Empty;
