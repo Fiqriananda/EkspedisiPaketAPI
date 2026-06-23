6cfc4de89b194699fdac1d47cacc1fcdb00f1e72
Delano <del.191006@gmail.com>
2026-06-23 14:32:09 +0700

    expedisi fairuz

--- Data/ExpedisiDbContext.cs
+++ Data/ExpedisiDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ExpedisiPaketAPI.Data
+namespace ExpedisiPaketAPI.Data //expedisi
 {
     public class ExpedisiDbContext : IdentityDbContext<IdentityUser>
     {
